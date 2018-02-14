a51a5b5a610a1b2458e8c7ba00f2d20481e16806
juanArias8 <juasda96@gmail.com>
2018-02-14 00:31:50 -0500

    correction php file

--- AjaxjQ/checkUser.php
+++ AjaxjQ/checkUser.php
@@ -1,7 +1,7 @@
 <?php
 	$user = $_GET['user'];
 	$con = mysqli_connect("localhost", "user", "password", "bdusuario");
-	$result = mysqli_query($con, "select * from usuario where nombre = '".$user."'");
+	$result = mysqli_query($con, "select * from user where name = '".$user."'");
 	if (mysqli_num_rows($result) > 0)
 		echo "1";
 	else echo "0";
